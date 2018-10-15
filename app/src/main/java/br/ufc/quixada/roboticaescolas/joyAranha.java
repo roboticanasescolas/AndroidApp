e1e2d086dddc286af59c029f7bb1976dc50bf66d
felipesilva543 <felipesilva543@alu.ufc.br>
2018-10-15 16:06:21 -0300

    "Ajuste de botões e adição de variaveis de posição inicial e começo de função para enviar comando"

--- app/src/main/java/br/ufc/quixada/roboticaescolas/joyAranha.java
+++ app/src/main/java/br/ufc/quixada/roboticaescolas/joyAranha.java
@@ -4,12 +4,16 @@ import android.bluetooth.BluetoothSocket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.example.enoque_alves.roboticaescolas.R;
 
 import java.io.OutputStream;
+import java.util.concurrent.TimeUnit;
 
 public class joyAranha extends AppCompatActivity {
 
@@ -20,11 +24,23 @@ public class joyAranha extends AppCompatActivity {
 
     private SeekBar sBa, sBb, sBc, sBd, sBe, sBf, sBg, sBh;
     private TextView tVa1, tVa2, tVb1, tVb2, tVc1, tVc2, tVd1, tVd2;
+    private Button reset;
+    private ImageButton conectar;
+    private int pIa, pIb, pIc, pId, pIe, pIf, pIg, pIh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joy_aranha);
+
+        pIa = 50;
+        pIb = 50;
+        pIc = 50;
+        pId = 50;
+        pIe = 50;
+        pIf = 50;
+        pIg = 50;
+        pIh = 50;
 
         sBa = (SeekBar) findViewById(R.id.seekBar_a1);
         sBb = (SeekBar) findViewById(R.id.seekBar_a2);
@@ -44,6 +60,44 @@ public class joyAranha extends AppCompatActivity {
         tVd1 = (TextView) findViewById(R.id.tVd1);
         tVd2 = (TextView) findViewById(R.id.tVd2);
 
+        reset = (Button) findViewById(R.id.bReset);
+
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sBa.setProgress(pIa);
+                sBb.setProgress(pIb);
+                sBc.setProgress(pIc);
+                sBd.setProgress(pId);
+                sBe.setProgress(pIe);
+                sBf.setProgress(pIf);
+                sBg.setProgress(pIg);
+                sBh.setProgress(pIh);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(300);
+                    enviarComando("!a" + pIa);
+                    TimeUnit.MILLISECONDS.sleep(300);
+                    enviarComando("!b" + pIb);
+                    TimeUnit.MILLISECONDS.sleep(300);
+                    enviarComando("!c" + pIc);
+                    TimeUnit.MILLISECONDS.sleep(300);
+                    enviarComando("!d" + pId);
+                    TimeUnit.MILLISECONDS.sleep(300);
+                    enviarComando("!e" + pIe);
+                    TimeUnit.MILLISECONDS.sleep(300);
+                    enviarComando("!f" + pIf);
+                    TimeUnit.MILLISECONDS.sleep(300);
+                    enviarComando("!g" + pIg);
+                    TimeUnit.MILLISECONDS.sleep(300);
+                    enviarComando("!h" + pIh);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
 
         sBa.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -59,9 +113,7 @@ public class joyAranha extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 String comando = "!a" + (seekBar.getProgress());
-                tVa1.setText("A1: " + seekBar.getProgress());
-                //enviaDados.enviarComando(comando);
-              //  enviarComando(comando);
+                enviarComando(comando);
             }
         });
         sBb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -77,9 +129,8 @@ public class joyAranha extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //String comando = "!a" + (seekBar.getProgress());
-                tVa2.setText("A2: " + seekBar.getProgress());
-                //  enviarComando(comando);
+                String comando = "!b" + (seekBar.getProgress());
+                enviarComando(comando);
             }
         });
 
@@ -96,9 +147,8 @@ public class joyAranha extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //String comando = "!a" + (seekBar.getProgress());
-                tVb2.setText("B2: " + seekBar.getProgress());
-                //  enviarComando(comando);
+                String comando = "!c" + (seekBar.getProgress());
+                enviarComando(comando);
             }
         });
         sBd.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -114,9 +164,8 @@ public class joyAranha extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //String comando = "!a" + (seekBar.getProgress());
-                tVb1.setText("B1: " + seekBar.getProgress());
-                //  enviarComando(comando);
+                String comando = "!d" + (seekBar.getProgress());
+                enviarComando(comando);
             }
         });
 
@@ -134,9 +183,8 @@ public class joyAranha extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //String comando = "!a" + (seekBar.getProgress());
-                tVc1.setText("C1: " + seekBar.getProgress());
-                //  enviarComando(comando);
+                String comando = "!e" + (seekBar.getProgress());
+                enviarComando(comando);
             }
         });
 
@@ -153,9 +201,8 @@ public class joyAranha extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //String comando = "!a" + (seekBar.getProgress());
-                tVc2.setText("C2: " + seekBar.getProgress());
-                //  enviarComando(comando);
+                String comando = "!f" + (seekBar.getProgress());
+                enviarComando(comando);
             }
         });
         sBg.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -171,9 +218,8 @@ public class joyAranha extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //String comando = "!a" + (seekBar.getProgress());
-                tVd2.setText("D2: " + seekBar.getProgress());
-                //  enviarComando(comando);
+                String comando = "!g" + (seekBar.getProgress());
+                enviarComando(comando);
             }
         });
         sBh.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -189,59 +235,47 @@ public class joyAranha extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //String comando = "!a" + (seekBar.getProgress());
-                tVd1.setText("D1: " + seekBar.getProgress());
-                //  enviarComando(comando);
+                String comando = "!h" + (seekBar.getProgress());
+                enviarComando(comando);
             }
         });
-
-
-
-
     }
 
- //   public void enviarComando (String comando){
-//        char chars [] = comando.toCharArray();
-//        char servo = chars[1];
-//        int auxBase = 0;
-//        String aux = "";
-//        for (int j = 2 ; j < chars.length; j++) {
-//            aux = aux + chars[j];
-//        }
-//        int posicaoAtual = Integer.parseInt(aux);
-//        if (servo == 'a'){
-//            posicaoAtual = (int) (posicaoAtual);
-//            tVa1.setText(""+ posicaoAtual);
-//        }
-//        else if (servo == 'b'){//controlar visao
-//            posicaoAtual = (int) (posicaoAtual);
-//            tVa2.setText(""+ posicaoAtual);
-//        }
-//        else if (servo == 'c'){
-//            posicaoAtual = (int) (posicaoAtual);
-//            tVb1.setText(""+posicaoAtual);
-//        }
-//        else if (servo == 'd'){
-//            posicaoAtual = (int) (posicaoAtual);
-//            tVb2.setText(""+ posicaoAtual);
-//        }
-//        else if (servo == 'e'){
-//            posicaoAtual = (int) (posicaoAtual);
-//            tVc1.setText(""+ posicaoAtual);
-//        }
-//        else if (servo == 'f'){
-//            posicaoAtual = (int) (posicaoAtual);
-//            tVc2.setText(""+ posicaoAtual);
-//        }
-//        else if (servo == 'g'){
-//            posicaoAtual = (int) (posicaoAtual);
-//            tVd1.setText(""+ posicaoAtual);
-//        }
-//        else if (servo == 'h'){
-//            posicaoAtual = (int) (posicaoAtual);
-//            tVd2.setText(""+ posicaoAtual);
-//        }
-    //    enviaDados.enviarComando(comando);
-//
-   // }
+   public void enviarComando (String comando){
+        char chars [] = comando.toCharArray();
+        char servo = chars[1];
+        int auxBase = 0;
+        String aux = "";
+        for (int j = 2 ; j < chars.length; j++) {
+            aux = aux + chars[j];
+        }
+        int posicaoAtual = Integer.parseInt(aux);
+        if (servo == 'a'){
+            tVa1.setText("A1: " + aux);
+            //Log.i("asd", "A1 - " + comando);
+        }
+        else if (servo == 'b'){//controlar visao
+            tVa2.setText("A2: " + aux);
+        }
+        else if (servo == 'c'){
+            tVb2.setText("B2: " + aux);
+        }
+        else if (servo == 'd'){
+            tVb1.setText("B1: " + aux);
+        }
+        else if (servo == 'e'){
+            tVc1.setText("C1: " + aux);
+        }
+        else if (servo == 'f'){
+            tVc2.setText("C2: " + aux);
+        }
+        else if (servo == 'g'){
+            tVd2.setText("D2: " + aux);
+        }
+        else if (servo == 'h'){
+            tVd1.setText("D1: " + aux);
+        }
+//        enviaDados.enviarComando(comando);
+
+    }
 }
